c09f024a05949e1ab68a623ebd5d2e91f7260414
Tester <120099770+1212312312312@users.noreply.github.com>
2024-04-18 20:23:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const toggleButton = document.getElementById('toggleButton');
 
+toggleButton.addEventListener('change', function() {
+    if (this.checked) {
+        // Change background color to red when off
+        document.body.style.backgroundColor = 'red';
+        // Vibrate phone when button is pressed
+        navigator.vibrate([100]);
+        // Continuously vibrate phone when button is on
+        setInterval(() => {
+            navigator.vibrate([100]);
+        }, 1000);
+    } else {
+        // Change background color to blue when off
+        document.body.style.backgroundColor = 'blue';
+        // Stop continuous vibration
+        clearInterval();
+    }
+});
