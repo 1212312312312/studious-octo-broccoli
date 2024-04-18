c10533283d0a1d20b558d2714a75cdb890765b94
Tester <120099770+1212312312312@users.noreply.github.com>
2024-04-18 20:42:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ toggleButton.addEventListener('change', function() {
     if (this.checked) {
         // Change background color to red when off
         document.body.style.backgroundColor = 'red';
-        // Vibrate phone when button is pressed
-        navigator.vibrate([100]);
-        // Continuously vibrate phone when button is on
-        setInterval(() => {
+        // Check if the device supports vibration
+        if ("vibrate" in navigator) {
+            // Vibrate phone when button is pressed
             navigator.vibrate([100]);
-        }, 1000);
+            // Continuously vibrate phone when button is on
+            const vibrationInterval = setInterval(() => {
+                navigator.vibrate([100]);
+            }, 1000);
+            // Store the interval ID to clear it later
+            this.dataset.vibrationInterval = vibrationInterval;
+        }
     } else {
         // Change background color to blue when off
         document.body.style.backgroundColor = 'blue';
         // Stop continuous vibration
-        clearInterval();
+        const vibrationInterval = parseInt(this.dataset.vibrationInterval);
+        clearInterval(vibrationInterval);
     }
 });
